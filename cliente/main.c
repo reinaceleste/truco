331697e329c5d5e0fd508ef19995e8eb748ac043
Federico Marinzalda <fmarinzalda@gmail.com>
2017-02-03 00:15:17 -0300

    Avances en cliente al 03/02.

--- cliente/main.c
+++ cliente/main.c
@@ -4,7 +4,7 @@
 *  \brief TP Obligatorio
 * \details Cliente
 * Uso del make
-* - Compilar con make new o make cliente
+* - Compilar con make new o make client
 * - Ejecutar o compilar y ejecutar en un paso com make run o simplemente make (luego de la ejecución se borrarán los archivos objeto y el ejecutable)
 * - Generar y abrir página web con documento Doxygen con make html
 * - Borrar web con make no_html
